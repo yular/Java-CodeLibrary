c8339d602c15e80c2ccd6a0752dc6ec770a4dfdd
Baichuan Yang <1062969706@qq.com>
2016-06-18 23:54:30 -0700

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.Socket.*;
 
 public class SocketClient {
 	public static void main(String[] args){
