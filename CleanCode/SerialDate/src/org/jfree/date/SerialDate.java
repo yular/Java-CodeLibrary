f2fde70117779153a189a6b795d560ff7f24dc08
yular <1062969706@qq.com>
2017-08-25 01:03:24 -0700

    Add SerialDate project discussed in Clean Code book

---
+++ CleanCode/SerialDate/src/org/jfree/date/SerialDate.java
@@ -0,0 +1,15 @@
+package org.jfree.date;
+
+
+import java.io.Serializable;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+
+public abstract class SerialDate implements Comparable, Serializable {
+
+    /** For serialization */
+    private static final long serialVersionUID = -293716040467423637L;
+
+    /** Date format symbols */
+    public static final DateFormatSymbols DATE_FORMAT_SYMBOLS = new SimpleDateFormat().getDateFormatSymbols();
+}
